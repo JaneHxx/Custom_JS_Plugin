816db624cb96da398dcdd186adeb458baf83d4fb
绿色小水杯 <2667225904@qq.com>
2019-08-23 16:13:05 +0800

    1、日历功能完善；

--- 自定义日期插件/js/calendar.js
+++ 自定义日期插件/js/calendar.js
@@ -2,7 +2,9 @@
     var monthDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
     function DCalendar (elem, options) {
         var that = this;
-        var _default = {};
+        var _default = {
+            onClick: null
+        };
         that.elem = $(elem);
         that.options = $.extend({}, _default, options);
         that.calendar = null;
@@ -10,16 +12,30 @@
         that.date = that.formatDate(that.today);
         that.create();
         that.getNewMonths();
-        that.calendar.find('.calendar-header .prev').on('click', function () {
+        that.calendar.find('.calendar-header').on('click', '.prev', function () {
            that.prevMonthDays();
         });
-        that.calendar.find('.calendar-header .next').on('click', function () {
+        that.calendar.find('.calendar-header').on('click', '.next', function () {
             that.nextMonthDays();
+        });
+        that.calendar.find('.calendar-dates').on('click', 'span', function (event) {
+            that.getCurrentDate(event);
         });
     }
 
     DCalendar.prototype = {
         constructor: DCalendar,
+        // 获取当前日期
+        getCurrentDate: function (event) {
+            var that = this;
+            event = window.event || event;
+            var target = event.target;
+            var day = target.getAttribute('day');
+            var month = that.today.getMonth();
+            var year = that.today.getFullYear();
+            var clkDates = new Date(year, month, day);
+            that.options.onClick(that.formatDate(clkDates));
+        },
         // 上一个月
         prevMonthDays: function () {
             var that = this;
@@ -71,8 +87,10 @@
         formatDate: function (date) {
             var vdate = new Date(date.getFullYear(), date.getMonth(), date.getDate());
             var year = vdate.getFullYear(),
-                month = vdate.getMonth() + 1,
+                month = vdate.getMonth(),
                 day = vdate.getDate();
+            month = month + 1 < 10 ? "0" + (month + 1): (month + 1);
+            day = day < 10 ? "0" + day : day;
             return year + '-' + month + '-' + day;
         },
         /* Sets current view based on user interaction (on arrows) */
@@ -90,22 +108,22 @@
             var that = this,
                 nDate = new Date(newDate.getFullYear(), newDate.getMonth(), newDate.getDate());
             that.calcFebDays(nDate);
-            var date = nDate.getDate(), days = monthDays[nDate.getMonth()];
+            var nowDate = new Date(), days = monthDays[nDate.getMonth()];
             // 获取当前月的第一天是星期几
             nDate.setDate(1);
             var dayWeek = nDate.getDay();
             var _html = '';
             for (var i = 0; i < Math.ceil(Number(days + dayWeek)/7); i++) {
                 for (var j = 1; j <= 7; j++) {
-                    if ((i * 7 + j <= dayWeek) || Number(i * 7 + j - dayWeek) > days) {
-                        _html += '<span>&nbsp;</span>';
-                    } else {
-                        if (j == 7 && Number(i * 7 + j - dayWeek + 1) == date) { _html += '<span class="active no-bd-right">'; }
-                        else if (Number(i * 7 + j - dayWeek + 1) == date) { _html += '<span class="active">'; }
-                        else if (j == 7) { _html += '<span class="no-bd-right">'; }
-                        else _html += '<span>';
-                        _html += Number(i * 7 + j - dayWeek) +'</span>';
+                    var day = Number(i * 7 + j - dayWeek);
+                    var dayElem = $('<span day="'+day+'">&nbsp;</span>');
+                    if ((i * 7 + j <= dayWeek) || day > days) {} else {
+                        if (j == 7 && (day == nowDate.getDate() && nDate.getMonth() == nowDate.getMonth() && nDate.getFullYear() == nowDate.getFullYear())) dayElem.addClass('active no-bd-right');
+                        else if ((day == nowDate.getDate() && nDate.getMonth() == nowDate.getMonth() && nDate.getFullYear() == nowDate.getFullYear())) dayElem.addClass('active');
+                        else if (j == 7) dayElem.addClass('no-bd-right');
+                        dayElem.text(day);
                     }
+                    _html += dayElem.prop('outerHTML');
                 }
             }
             callback(_html);
